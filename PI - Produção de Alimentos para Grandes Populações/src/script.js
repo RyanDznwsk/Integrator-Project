eeca5b9d8464ab8c095db3c3fd8940182e3b07fe
Ryan <ryan.gduzanowski@gmail.com>
2026-07-26 04:47:32 -0300

    Add files via upload

--- PI - Produção de Alimentos para Grandes Populações/src/script.js
+++ PI - Produção de Alimentos para Grandes Populações/src/script.js
@@ -1,3 +1,5 @@
+// Scroll Reveal
+
 const configBase = {
   duration: 500,
   easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
@@ -12,7 +14,7 @@ ScrollReveal().reveal('.nav > a', {
   delay: 100
 });
 
-ScrollReveal().reveal('.navbar li', {
+ScrollReveal().reveal('.links li', {
   ...configBase,
   origin: 'top',
   interval: 50,
@@ -27,6 +29,7 @@ ScrollReveal().reveal('.hero-text .revelar-baixo', {
   interval: 220,
   delay: 400
 });
+
 ScrollReveal().reveal('.hero-text .btn-reveal', {
   ...configBase,
   origin: 'bottom',
@@ -83,3 +86,88 @@ ScrollReveal().reveal('.text-reveal', {
   scale: 0.95,
   delay: 400
 });
+
+ScrollReveal().reveal('.banner-text .revelar-baixo', {
+  ...configBase,
+  origin: 'bottom',
+  scale: 0.95,
+  interval: 220,
+  delay: 400
+});
+
+ScrollReveal().reveal('.tec-revo-text', {
+  ...configBase,
+  origin: 'bottom',
+  scale: 0.95,
+  delay: 150
+});
+
+ScrollReveal().reveal('.tec-revo-cards .tec-revo-card-reveal', {
+  ...configBase,
+  origin: 'bottom',
+  scale: 0.9,
+  easing: 'cubic-bezier(0.5, 1.8, 0.3, 0.8)',
+  interval: 150, 
+  delay: 100
+});
+
+ScrollReveal().reveal('.evo-text', {
+  ...configBase,
+  origin: 'bottom',
+  scale: 0.95,
+  delay: 200
+});
+
+ScrollReveal().reveal('.left-time', {
+  ...configBase,
+  origin: 'left',
+  scale: 0.95,
+  easing: 'cubic-bezier(0.5, 1.8, 0.3, 0.8)', 
+  delay: 200
+});
+
+ScrollReveal().reveal('.right-time', {
+  ...configBase,
+  duration: 700,
+  origin: 'right',
+  scale: 0.95,
+  easing: 'cubic-bezier(0.5, 1.8, 0.3, 0.8)', 
+  delay: 300
+});
+
+ScrollReveal().reveal('.videos-educacionais', {
+  ...configBase,
+  origin: 'left',
+  scale: 0.95,
+  delay: 200
+});
+
+ScrollReveal().reveal('.videos-carrossel', {
+  ...configBase,
+  origin: 'right',
+  scale: 0.95,
+  delay: 500
+});
+
+// Carrossel de Vídeos
+
+let track = document.getElementById('track');
+let nextBtn = document.getElementById('next-btn');
+let slides = document.querySelectorAll('.carrossel-item');
+let index = 0;
+function updateCarousel() {
+  const width = document.querySelector('.carrossel-slide').clientWidth;
+  track.style.transform = `translateX(${-index * width}px)`;
+  let nativeVideos = track.getElementsByTagName('video');
+  for (let video of nativeVideos) {
+    video.pause();
+  }
+}
+nextBtn.addEventListener('click', () => {
+  index++;
+  if (index >= slides.length) {
+    index = 0;
+  }
+  updateCarousel();
+});
+window.addEventListener('resize', updateCarousel);
